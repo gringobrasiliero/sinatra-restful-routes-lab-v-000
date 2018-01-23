8910ce534e55882130d04b4713c341510fb01a0e
Nolan Byrnes <nolanehs@gmail.com>
2018-01-23 14:31:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,36 @@
 class ApplicationController < Sinatra::Base
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
     erb :index
   end
+
+get '/recipes' do
+  @recipes = Recipe.all
+  erb :index
 end
+
+get '/recipes/:id' do
+  @recipe = Recipe.find_by_id(params[:id])
+  erb :show
+end
+
+get '/recipes/:id/edit' do
+  @recipe = Recipe.find_by_id(params[:id])
+  erb :edit
+end
+
+
+
+delete '/recipes/:id/delete' do
+  @recipe = Recipe.find_by_id(params[:id])
+  @recipe.delete
+  redirect to '/recipes'
+end
+
+
+end #of class
